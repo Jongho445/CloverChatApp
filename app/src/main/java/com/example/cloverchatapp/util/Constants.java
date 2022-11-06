5adf60d0cbdebc18688acc9d10c62b45f77aada6
Jongho Kim <zc798546@gmail.com>
2022-11-06 20:47:41 +0900

    Change backend server IP

--- app/src/main/java/com/example/cloverchatapp/util/Constants.java
+++ app/src/main/java/com/example/cloverchatapp/util/Constants.java
@@ -3,5 +3,5 @@ package com.example.cloverchatapp.util;
 public class Constants {
 
 //    public static String SERVER_URL = "http://10.0.2.2:11730";
-    public static String SERVER_URL = "http://141.164.51.220:11730";
+    public static String SERVER_URL = "http://141.164.47.166:11730";
 }
