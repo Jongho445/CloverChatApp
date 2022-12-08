739284858328275cd1fe0d5851d1c8b3eabe991d
Jongho Kim <zc798546@gmail.com>
2022-12-08 20:26:04 +0900

    Change server url

--- app/src/main/java/com/example/cloverchatapp/util/Constants.java
+++ app/src/main/java/com/example/cloverchatapp/util/Constants.java
@@ -2,6 +2,6 @@ package com.example.cloverchatapp.util;
 
 public class Constants {
 
-//    public static String SERVER_URL = "http://10.0.2.2:11730";
-    public static String SERVER_URL = "http://141.164.35.13:11730";
+    public static String SERVER_URL = "http://10.0.2.2:11730";
+//    public static String SERVER_URL = "http://141.164.35.13:11730";
 }
